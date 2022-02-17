ec7865bbde58ff95855b0f9e2b449de1533d7a6c
visciang <giovanni_visciano@yahoo.it>
2022-02-17 22:43:09 +0100

    no supervisors (#3)

--- lib/exql/migration.ex
+++ lib/exql/migration.ex
@@ -1,43 +1,138 @@
-# coveralls-ignore-start
-
-defmodule Exql.Migration.Migration do
-  @moduledoc "Migration runner Supervisor"
-
-  use Supervisor
+defmodule Exql.Migration do
+  @moduledoc false
 
   require Logger
+  alias Exql.Migration.{Log, Schema}
 
-  @type opts :: [
-          {:conn, Postgrex.conn()}
-          | {:migrations_dir, Path.t()}
-          | {:timeout, timeout()}
-          | {:transactional, boolean()}
+  @type credentials :: [
+          hostname: String.t(),
+          username: String.t(),
+          password: String.t(),
+          database: String.t()
         ]
 
-  @spec start_link(opts()) :: Supervisor.on_start()
-  def start_link(opts) do
-    conn = Keyword.fetch!(opts, :conn)
-    migrations_dir = Keyword.fetch!(opts, :migrations_dir)
-    timeout = Keyword.get(opts, :timeout, :infinity)
-    transactional = Keyword.get(opts, :transactional, true)
+  @spec create_db(credentials(), String.t()) :: :ok
+  def create_db(credentials, db_name) do
+    start_options = Keyword.put(credentials, :pool_size, 1)
+    {:ok, conn} = Postgrex.start_link(start_options)
 
-    Supervisor.start_link(__MODULE__, {conn, migrations_dir, timeout, transactional})
-  end
+    unless exists_db?(conn, db_name) do
+      case Postgrex.query(conn, "create database #{db_name}", []) do
+        {:ok, _} ->
+          Logger.info("Created DB #{inspect(db_name)}")
 
-  @impl Supervisor
-  @spec init({Postgrex.conn(), Path.t(), timeout(), boolean()}) :: :ignore
-  def init({conn, migrations_dir, timeout, transactional}) do
-    Exql.Migration.migrate(conn, migrations_dir, timeout, transactional)
+        # coveralls-ignore-start
+        {:error, %Postgrex.Error{postgres: %{code: :duplicate_database}}} ->
+          # if two instances of the app runs concurrently, exists_db? + query!
+          # could raise since we do not acquire locks and execute these two
+          # operations transactionally (create database can't run in a transaction)
+          :ok
 
-    :ignore
+          # coveralls-ignore-end
+      end
+    end
+
+    GenServer.stop(conn)
   rescue
     exc ->
+      # coveralls-ignore-start
+      Logger.emergency("Create DB failed")
+      Logger.emergency(Exception.message(exc))
+      Logger.emergency("#{inspect(exc)}")
+
+      :init.stop()
+      # coveralls-ignore-end
+  end
+
+  @spec migrate(credentials(), Path.t(), boolean(), timeout()) :: :ok
+  def migrate(credentials, migrations_dir, transactional \\ true, timeout \\ :infinity) do
+    start_options = Keyword.put(credentials, :pool_size, 1)
+    {:ok, conn} = Postgrex.start_link(start_options)
+
+    Schema.setup(conn)
+
+    Logger.info("Migration started (dir: #{inspect(migrations_dir)})")
+
+    migrations_dir
+    |> migration_files()
+    |> filter_migrations(Log.last_migration(conn))
+    |> sort_migrations()
+    |> Enum.each(&run(conn, &1, File.read!(Path.join(migrations_dir, &1)), timeout, transactional))
+
+    Logger.info("Migration completed")
+
+    GenServer.stop(conn)
+  rescue
+    exc ->
+      # coveralls-ignore-start
       Logger.emergency("Migration failed")
       Logger.emergency(Exception.message(exc))
       Logger.emergency("#{inspect(exc)}")
 
       :init.stop()
+      # coveralls-ignore-end
+  end
+
+  @spec run(Postgrex.conn(), Log.migration_id(), String.t(), timeout(), boolean()) :: :ok
+  defp run(conn, migration_id, statements, timeout, transactional) do
+    Logger.info("[#{migration_id}] Running")
+
+    statements = """
+    do $$
+    begin
+      #{statements}
+    end $$
+    """
+
+    if transactional do
+      Postgrex.transaction(
+        conn,
+        fn conn ->
+          Log.lock(conn)
+          run_statements(conn, migration_id, statements, :infinity)
+        end,
+        timeout: timeout
+      )
+    else
+      run_statements(conn, migration_id, statements, timeout)
+    end
+
+    Logger.info("[#{migration_id}] Completed")
+
+    :ok
+  end
+
+  @spec run_statements(Postgrex.conn(), Log.migration_id(), String.t(), timeout()) :: :ok
+  defp run_statements(conn, migration_id, statements, timeout) do
+    Logger.debug(statements)
+    Postgrex.query!(conn, statements, [], timeout: timeout)
+    shasum = :crypto.hash(:sha256, statements) |> Base.encode16(case: :lower)
+    Log.insert(conn, migration_id, shasum)
+    :ok
+  end
+
+  @spec migration_files(Path.t()) :: [String.t()]
+  defp migration_files(migrations_dir) do
+    migrations_dir
+    |> File.ls!()
+    |> Enum.filter(&String.ends_with?(&1, ".sql"))
+  end
+
+  @spec filter_migrations([String.t()], Log.migration_id()) :: [String.t()]
+  defp filter_migrations(migrations, last_migration),
+    do: Enum.reject(migrations, &applied?(&1, last_migration))
+
+  @spec sort_migrations([String.t()]) :: [String.t()]
+  defp sort_migrations(migrations),
+    do: Enum.sort(migrations)
+
+  @spec applied?(Log.migration_id(), nil | Log.migration_id()) :: boolean()
+  defp applied?(migration, last_applied),
+    do: migration <= last_applied
+
+  @spec exists_db?(Postgrex.conn(), String.t()) :: boolean()
+  defp exists_db?(conn, name) do
+    res = Postgrex.query!(conn, "select true from pg_database where datname = $1", [name])
+    match?(%Postgrex.Result{num_rows: 1}, res)
   end
 end
-
-# coveralls-ignore-stop
--- test/exql_migration_test.exs
+++ test/exql_migration_test.exs
@@ -2,69 +2,67 @@ defmodule Test.Exql.Migration do
   use ExUnit.Case
   alias Exql.Migration
 
-  @postgrex_conn :test
   @timeout 1_000
 
   @postgrex_conf [
     hostname: System.get_env("POSTGRES_HOST", "localhost"),
     username: "postgres",
     password: "postgres",
-    database: "postgres",
-    name: @postgrex_conn
+    database: "postgres"
   ]
 
   setup_all do
-    start_supervised!({Postgrex, @postgrex_conf})
-    :ok
+    conn = start_supervised!({Postgrex, @postgrex_conf})
+    %{conn: conn}
   end
 
-  setup do
+  setup %{conn: conn} do
     on_exit(fn ->
-      Postgrex.query!(@postgrex_conn, "drop schema if exists exql_migration cascade", [])
-      Postgrex.query!(@postgrex_conn, "drop schema if exists test cascade", [])
+      Postgrex.query!(conn, "drop schema if exists exql_migration cascade", [])
+      Postgrex.query!(conn, "drop schema if exists test cascade", [])
     end)
 
     :ok
   end
 
-  test "empty migrations set" do
-    Migration.migrate(@postgrex_conn, "test/no_migrations", @timeout, true)
-    assert Migration.Log.migrations(@postgrex_conn) == []
+  test "empty migrations set", %{conn: conn} do
+    Migration.migrate(@postgrex_conf, "test/no_migrations", true, @timeout)
+    assert Migration.Log.migrations(conn) == []
   end
 
-  test "add a new migration" do
-    Migration.migrate(@postgrex_conn, "test/partial_migrations", @timeout, true)
-    assert [%{id: "001.sql"}] = Migration.Log.migrations(@postgrex_conn)
+  test "add a new migration", %{conn: conn} do
+    Migration.migrate(@postgrex_conf, "test/partial_migrations", true, @timeout)
+    assert [%{id: "001.sql"}] = Migration.Log.migrations(conn)
 
-    Migration.migrate(@postgrex_conn, "test/all_migrations", @timeout, true)
-    assert [%{id: "001.sql"}, %{id: "002.sql"}] = Migration.Log.migrations(@postgrex_conn)
+    Migration.migrate(@postgrex_conf, "test/all_migrations", true, @timeout)
+    assert [%{id: "001.sql"}, %{id: "002.sql"}] = Migration.Log.migrations(conn)
   end
 
-  test "add a new migration (non transactional)" do
-    Migration.migrate(@postgrex_conn, "test/partial_migrations", @timeout, false)
-    assert [%{id: "001.sql"}] = Migration.Log.migrations(@postgrex_conn)
+  test "add a new migration (non transactional)", %{conn: conn} do
+    Migration.migrate(@postgrex_conf, "test/partial_migrations", false, @timeout)
+    assert [%{id: "001.sql"}] = Migration.Log.migrations(conn)
 
-    Migration.migrate(@postgrex_conn, "test/all_migrations", @timeout, false)
-    assert [%{id: "001.sql"}, %{id: "002.sql"}] = Migration.Log.migrations(@postgrex_conn)
+    Migration.migrate(@postgrex_conf, "test/all_migrations", false, @timeout)
+    assert [%{id: "001.sql"}, %{id: "002.sql"}] = Migration.Log.migrations(conn)
   end
 
-  test "idempotent" do
-    Migration.migrate(@postgrex_conn, "test/all_migrations", @timeout, true)
-    assert all = [%{id: "001.sql"}, %{id: "002.sql"}] = Migration.Log.migrations(@postgrex_conn)
+  test "idempotent", %{conn: conn} do
+    Migration.migrate(@postgrex_conf, "test/all_migrations", true, @timeout)
+    assert all = [%{id: "001.sql"}, %{id: "002.sql"}] = Migration.Log.migrations(conn)
 
-    Migration.migrate(@postgrex_conn, "test/all_migrations", @timeout, true)
-    assert ^all = Migration.Log.migrations(@postgrex_conn)
+    Migration.migrate(@postgrex_conf, "test/all_migrations", true, @timeout)
+    assert ^all = Migration.Log.migrations(conn)
   end
 
-  test "create DB" do
+  test "create DB", %{conn: conn} do
     on_exit(fn ->
-      Postgrex.query!(@postgrex_conn, "drop database test_db", [])
+      Postgrex.query!(conn, "drop database test_db", [])
     end)
 
-    Migration.create_db(@postgrex_conn, "test_db")
-    Migration.create_db(@postgrex_conn, "test_db")
+    Migration.create_db(@postgrex_conf, "test_db")
+    Migration.create_db(@postgrex_conf, "test_db")
 
-    res = Postgrex.query!(@postgrex_conn, "select true from pg_database where datname = $1", ["test_db"])
+    res = Postgrex.query!(conn, "select true from pg_database where datname = $1", ["test_db"])
     assert %Postgrex.Result{num_rows: 1} = res
   end
 end
